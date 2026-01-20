70da9304b4511a88edb0435c79f8b4b004b1b0b3
KRI <knyazevseven@gmail.com>
2026-01-20 21:37:23 +0300

    context

--- ultima/context.go
+++ ultima/context.go
@@ -1,1 +1,25 @@
 package ultima
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Context struct {
+	w http.ResponseWriter
+	r *http.Request
+}
+
+func NewContext(w http.ResponseWriter, r *http.Request) *Context {
+	return &Context{
+		w: w,
+		r: r,
+	}
+}
+
+func (c *Context) Json(i any, status int) *Context {
+	c.w.Header().Set("Content-Type", "application/json")
+	c.w.WriteHeader(status)
+	json.NewEncoder(c.w).Encode(i)
+	return c
+}
